59a50a301cbfa47d402e81ebe4490144abd660ca
Oyinlade demola <demolinton4comin@gmail.com>
2026-07-19 18:31:30 +0100

    update

--- python-be/src/app.py
+++ python-be/src/app.py
@@ -1,3 +1,6 @@
+import glob
+import json
+
 from flask import Flask, request, jsonify
 import os
 import tempfile
@@ -23,7 +26,7 @@ def extract_pdf():
         output_dir = os.path.join(tmpdir, "output")
 
         os.makedirs(output_dir, exist_ok=True)
-
+       
         pdf_file.save(input_pdf)
         print(output_dir)
         try:
@@ -34,10 +37,17 @@ def extract_pdf():
             )
             print("Result:", result)
             print("Output files:", os.listdir(output_dir))
-
+            md_files = glob.glob(os.path.join(output_dir, "*.md"))
+            json_files = glob.glob(os.path.join(output_dir, "*.json"))
+            with open(json_files[0], "r", encoding="utf-8") as f:
+                data = json.load(f)
+            with open(md_files[0], "r", encoding="utf-8") as f:
+                markdown = f.read()
             return jsonify({
                 "status": "success",
-                "result": result
+                "result": result,
+                "data": data,
+                "markdown": markdown
             })
 
         except Exception as e:
